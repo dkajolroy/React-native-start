70f475223e253140fce2f7e9f18d36dc86754ff8
Kajol Roy <shrikajol@gmail.com>
2022-08-18 08:45:27 +0600

    react_native-first-setup

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <ScrollView>
+        <Text >
+          Digital School Of our kids
+        </Text>
+      </ScrollView>
+    </View >
+  )
+}
