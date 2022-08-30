4830189e8446866134601030ac17d460d020ea3d
Kajol Roy <shrikajol@gmail.com>
2022-08-30 21:03:33 +0600

    sounter value

--- App.js
+++ App.js
@@ -1,14 +1,50 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, StatusBar, StyleSheet, Button, TextInput } from 'react-native'
+import React, { useState } from 'react'
 
 export default function App() {
+  const [value, setValue] = useState(2)
+  
   return (
-    <View>
-      <ScrollView>
-        <Text >
-          Digital School Of our kids
-        </Text>
-      </ScrollView>
+    <View style={style.view}>
+      <StatusBar backgroundColor="green" />
+      <View style={style.box}>
+        <Button onPress={() => setValue(x=>x-2)} title='Incre' />
+        <Text style={style.value}>{value}</Text>
+        <Button onPress={() => setValue(x=>x+2)} title='Decre' />
+      </View>
+        <TextInput keyboardType='numeric' onChangeText={(e)=>setValue(e)} style={style.input} placeholder='Enter a value' />
     </View >
   )
 }
+
+const style = StyleSheet.create({
+
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "green"
+  },
+  box: {
+    width: 300,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderRadius: 50,
+    backgroundColor: "#fff"
+  },
+  value:{
+    fontSize: 22,
+    marginHorizontal: 10,
+  },
+  input:{
+    backgroundColor: "#fff",
+    borderRadius:10,
+    width:300,
+    fontSize: 20,
+    marginTop: 10,
+  }
+
+
+})
